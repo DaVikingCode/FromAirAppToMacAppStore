2b60952bcc47b2b95794bde0f8bffc569340e730
Aymeric <aymeric.lamboley@gmail.com>
2014-05-30 16:48:43 +0200

    import script

---
+++ FromAirToMacAppStore.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#!/usr/bin/env bash
+#$ -N $2
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+rm $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/Current/Resources/WebKit.dylib 
+
+chmod -R 777 $DIR/$1.app/
+
+codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/AdobeCP15.plugin
+codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/Flash\ Player.plugin
+codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/adobecp.plugin
+codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0
+codesign -f -v -s "$2" $DIR/$1.app
+
+productbuild --component $DIR/$1.app /Applications $DIR/$1.pkg --sign "$3"
+
+codesign -v --verify $DIR/$1.app
+
+exit 0
+
+#sh FromAirToMacAppStore.sh SillyFamilyDesktop "3rd Party Mac Developer Application: APPTOONOMY (XXXXXXXXXX)" "3rd Party Mac Developer Installer: APPTOONOMY (XXXXXXXXXX)"
