93eb611622358d871fce180c49fdcd0c79f2c3ca
Aymeric <aymeric.lamboley@gmail.com>
2014-06-02 11:18:40 +0200

    working build + instructions

--- FromAirToMacAppStore.sh
+++ FromAirToMacAppStore.sh
@@ -8,12 +8,15 @@ cp $DIR/Info.plist $DIR/$1.app/Contents
 cp $DIR/Icon.icns $DIR/$1.app/Contents/Resources
 
 rm $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/Current/Resources/WebKit.dylib
+rm -rf $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/AdobeCP15.plugin
+rm -rf $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/Flash\ Player.plugin
+rm -rf $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/adobecp.plugin
 
 chmod -R 777 $DIR/$1.app/
 
-codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/AdobeCP15.plugin
-codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/Flash\ Player.plugin
-codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/adobecp.plugin
+#codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/AdobeCP15.plugin
+#codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/Flash\ Player.plugin
+#codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0/Resources/adobecp.plugin
 codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework/Versions/1.0
 codesign -f -v -s "$2" $DIR/$1.app/Contents/Frameworks/Adobe\ AIR.framework
 codesign -f -v -s "$2" --entitlements $DIR/entitlements.plist $DIR/$1.app
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 FromAirAppToMacAppStore
 =======================
 
-A quick script for going on the Mac App Store from your Adobe Air Application.
+A quick script for going on the Mac App Store from your Adobe Air Application, used for [Silly Family](https://itunes.apple.com/us/app/silly-family/id882669738?).
+Thanks to this wonderful [guide](http://pigsels.com/2012/04/air-app-store-publishing-guide/) & Adobe's [article](http://helpx.adobe.com/flash-player/kb/posting-air-app-mac-app.html).
+
+Before running the script, be sure to add `<key>LSApplicationCategoryType</key>` in your *Info.plist* file, like we did.
+
+You will also have to generate an *icns* file. Replace files inside *myicon.iconset* folder and run `iconutil -c icns -o Icon.icns myicon.iconset` to create your icon.
+
+Now you can run the script:
+`sh FromAirToMacAppStore.sh SillyFamilyDesktop "3rd Party Mac Developer Application: DaVikingCode (XXXXXXXXXX)" "3rd Party Mac Developer Installer: DaVikingCode (XXXXXXXXXX)"`
